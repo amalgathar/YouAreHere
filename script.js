e77bbcebe147a189687652204f7b5c9f8269b0b4
JamesTown-625 <{JamesTown-625}@users.noreply.github.com>
2017-11-04 10:48:33 -0600

    Here is the initial Javascript for our google API

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("yo");
+$(document).on('click', '.searchButton', function() {
+  $('#searches').empty();
+  var type = $(this).data('type');
+  var apiKey = 'key=AIzaSyAB6pgrU8w2KcZEpSwlRmldkyCIxrBl0ts'
+  var queryURL = 'https://maps.googleapis.com/maps/api/js?' +apiKey+ "&callback=initMap" async defer;
+ $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
+
+
+      // // function initMap() {
+      //   var myLatLng = {lat: -25.363, lng: 131.044};
+
+      //   // Create a map object and specify the DOM element for display.
+      //   var map = new google.maps.Map(document.getElementById('map'), {
+      //     center: myLatLng,
+      //     zoom: 4
+      //   });
+
+      //   // Create a marker and set its position.
+      //   var marker = new google.maps.Marker({
+      //     map: map,
+      //     position: myLatLng,
+      //     title: 'Hello World!'
+      //   });
+      // }
